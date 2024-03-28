e91d3ed741068f28eac50697c4335fdf9726babc
Taziwa-Mushayabasa <tazmush@live.com>
2024-03-28 09:58:45 +0000

    Added login page, added database file, confirmed SQL injection

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname)); 
-
+app.use(express.json());
 
 const db = new sqlite3.Database('./database.sqlite', (err) => {
   if (err) {
@@ -15,10 +14,22 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
   console.log('Connected to the SQLite database.');
 });
 
-db.serialize(() => {
+db.run('CREATE TABLE IF NOT EXISTS wishlist (id INTEGER PRIMARY KEY AUTOINCREMENT, item TEXT NOT NULL)');
 
-  db.run('CREATE TABLE IF NOT EXISTS wishlist (id INTEGER PRIMARY KEY AUTOINCREMENT, item TEXT NOT NULL)');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
+
+app.post('/login', (req, res) => {
+
+  res.redirect('/index.html');
+});
+
+// Serve the wishlist page (index.html)
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 
 app.post('/wishlist', (req, res) => {
   const { item } = req.body;
@@ -26,18 +37,15 @@ app.post('/wishlist', (req, res) => {
   db.run(sql, [item], function(err) {
     if (err) {
       console.error(err.message);
-      res.status(500).send({ error: 'Error adding wishlist item' });
+      res.status(500).send('Error adding to wishlist');
     } else {
-      res.json({ success: 'Wishlist item added', id: this.lastID });
+
+      res.send('<script>alert("Wishlist item added"); window.location.href = "/index.html";</script>');
     }
   });
 });
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
